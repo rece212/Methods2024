633c0f96ddd1717bfb00345329069adde736a2f3
Reece Wanvig <rwanvig@iie.ac.za>
2024-02-26 13:24:26 +0200

    Add project files.

---
+++ prjMethods/Class1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prjMethods
+{
+     class Class1
+    {
+        public string Results(int Value1,int value2, int value3, int value4)
+        {
+            int Value = (Value1 + value2 + value3 + value4) / 4;
+            String strOutput = "Your result is " + Value + " meaning : ";
+            if (Value <= 48)
+            {
+                strOutput += "Failed";
+            }
+            else if (Value == 49)
+            {
+                strOutput += "Passed (condoned)";
+            }
+            else if (Value >= 50 && Value <= 73)
+            {
+                strOutput += "Passed";
+            }
+            else if (Value == 74)
+            {
+                strOutput += "Distinction (condoned)";
+            }
+            else if ((Value >= 75))
+            {
+                strOutput += "Distinction";
+            }
+
+            return strOutput;
+        }
+        public string Results(int Value)
+        {
+            String strOutput = "Your result is " + Value + " meaning : ";
+            if (Value <= 48)
+            {
+                strOutput += "Failed";
+            }
+            else if (Value == 49)
+            {
+                strOutput += "Passed (condoned)";
+            }
+            else if (Value >= 50 && Value <= 73)
+            {
+                strOutput += "Passed";
+            }
+            else if (Value == 74)
+            {
+                strOutput += "Distinction (condoned)";
+            }
+            else if ((Value >= 75))
+            {
+                strOutput += "Distinction";
+            }
+
+            return strOutput;
+        }
+    }
+}
---
+++ prjMethods/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace prjMethods
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+
+            Class1 class1 = new Class1();
+
+
+            Console.WriteLine( "Please enter your mark :" );
+            int iValue=Convert.ToInt32( Console.ReadLine());
+            String strOutput = class1.Results(iValue);
+            Console.WriteLine(strOutput);
+            strOutput = class1.Results(iValue,80,75,20);
+            Console.WriteLine(strOutput);
+
+        }
+    }
+}
